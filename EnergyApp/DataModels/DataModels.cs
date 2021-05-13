44216200373d15e33cfe4cb214e3bfe8d0132e93
Sourav Paul <srvpul@gmail.com>
2021-05-13 12:13:46 +0200

    Generation Report moved

--- EnergyApp/DataModels/DataModels.cs
+++ EnergyApp/DataModels/DataModels.cs
@@ -10,53 +10,10 @@ namespace EnergyApp.DataModels
 
     }
 
-    [Serializable]
-    public class Day
+    public class GenerationOutput
     {
-        [XmlElement("Date")]
-        public DateTime Date;
-        [XmlElement("Enerygy")]
-        public double Energy;
-        [XmlElement("Price")]
-        public double Price;
-    }
 
-    [Serializable]
-    public enum Location
-    {
-        NotSet = -1,
-        Onshore = 0,
-        Offshore = 1
     }
+    
 
-    public class GenerationReport
-    {
-        public List<WindGenerator> Wind;
-        public List<GasGenerator> Gas;
-        public List<CoalGenerator> Coal;
-    }
-
-    [Serializable]
-    public class EnergyGenerator
-    {
-        public string Name;
-
-        public List<Day> Generation;
-    }
-
-    public class WindGenerator : EnergyGenerator
-    {
-        public Location Location;
-    }
-    public class GasGenerator : EnergyGenerator
-    {
-        public double EmissionsRating;
-    }
-
-    public class CoalGenerator : EnergyGenerator
-    {
-        public double TotalHeatGeneration;
-        public double ActualNetGeneration;
-        public double EmissionsRating;
-    }
 }
---
+++ EnergyApp/DataModels/GenerationReport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnergyApp.DataModels
+{
+    public class Day
+    {
+        public DateTime Date;
+        public double Energy;
+        public double Price;
+    }
+
+    public enum Location
+    {
+        NotSet = -1,
+        Onshore = 0,
+        Offshore = 1
+    }
+
+    public class GenerationReport
+    {
+        public List<WindGenerator> Wind;
+        public List<GasGenerator> Gas;
+        public List<CoalGenerator> Coal;
+    }
+
+    public class EnergyGenerator
+    {
+        public string Name;
+
+        public List<Day> Generation;
+    }
+
+    public class WindGenerator : EnergyGenerator
+    {
+        public Location Location;
+    }
+    public class GasGenerator : EnergyGenerator
+    {
+        public double EmissionsRating;
+    }
+
+    public class CoalGenerator : EnergyGenerator
+    {
+        public double TotalHeatGeneration;
+        public double ActualNetGeneration;
+        public double EmissionsRating;
+    }
+}
