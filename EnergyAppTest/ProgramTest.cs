4544e990bf6ac1233cb4fb8315374e84e6dbce7b
Sourav Paul <srvpul@gmail.com>
2021-05-18 13:59:21 +0200

    Initial test suite set

---
+++ EnergyAppTest/ProgramTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnergyAppTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
